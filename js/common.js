0e24cfbcb1e21df388a63f61365739c2361ac6a6
lhbae <lh.bae@swinnus.com>
2018-03-27 17:35:24 +0900

    대상/단말 설명 추가

--- js/common.js
+++ js/common.js
@@ -459,7 +459,7 @@ function checkPopup(id) {
     };
 
 	var legend = {
-        title: '대상/단말기 상태 설명',
+        title: '대상/단말 상태 설명',
         width: '350',
         open: function () {
             $(this).html('<div class="form text-left legend-list">' +
@@ -472,7 +472,7 @@ function checkPopup(id) {
 				'</ul>' +
 				'</div>' +
 				'<div class="form text-left legend-list">' +
-                '<p>단말기 상태 아이콘</p>' +
+                '<p>단말 상태 아이콘</p>' +
 				'<ul class="item">' +
                 '<li data-value="0">데이터미수신</li>' +
                 '<li data-value="1">정상수신</li>' +
